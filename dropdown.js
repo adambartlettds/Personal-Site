af3e22865d3a5d83c183f4629b0b59ebfa874b4b
adambartlettds <88557970+adambartlettds@users.noreply.github.com>
2023-03-15 11:27:06 -0400

    Delete dropdown.js

--- dropdown.js
+++
@@ -1,8 +0,0 @@
-// Add interactivity to dropdown menus
-var dropdownToggle = document.querySelectorAll('.dropdown-toggle');
-
-dropdownToggle.forEach(function(el) {
-  el.addEventListener('click', function() {
-    this.nextElementSibling.classList.toggle('active');
-  });
-});
