d0c77f2045812da85b5ea3133f40e3df543cac6d
adambartlettds <88557970+adambartlettds@users.noreply.github.com>
2023-03-15 11:26:46 -0400

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// Add some interactivity to the page
-var menuToggle = document.querySelector('.menu-toggle');
-var nav = document.querySelector('nav');
-
-menuToggle.addEventListener('click', function() {
-  nav.classList.toggle('active');
-});
