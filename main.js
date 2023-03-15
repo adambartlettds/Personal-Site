94eb5ac845b33929603b357bd13276acd80f54de
adambartlettds <88557970+adambartlettds@users.noreply.github.com>
2023-03-15 11:56:52 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
+// main.js
 
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
